2ef50d3acd601da3eff56ee67a4d668977935e08
Kunal Aggarwal <kunalaggarwal58@gmail.com>
2018-12-20 10:20:47 +0000

    Hackerearth Stack: Frustated Coders

--- FrustatedCoders.py
+++ FrustatedCoders.py
@@ -1,26 +1,39 @@
+'''
+            Problem Statement:
+            
+            There are N frustrated coders standing in a circle with a gun in their hands. Each coder has a skill value S[ i ] and he can only kill those coders 
+            have strictly less skill than him. One more thing, all the guns have only 1 bullet. This roulette can take place in any random order. Fortunately, 
+            you have the time stone (haaan wo harre wala) and you can see all possible outcomes of this scenario. Find the outcome where the total sum of the 
+            remaining coder's skill is minimum. Print this sum.
+            
+'''
+
+
 from collections import deque
 n = int(input())
+list1 = []
+inp = input().split()
+for x in range(n):
+    list1.append(int(inp[x]))
 
-with open('input.txt', 'r') as fp:
-    inp = (fp.readline()).split()
-
-
-print(len(inp))
-list1 = [int(x) for x in inp]
-print(len(list1))
-list1.sort(reverse=True)
+list1.sort()
 list2 = list1.copy()
 list2.pop()
-
-
+sum=0
 for x in reversed(list1):
     
+    
     for y in reversed(list2):
-        if x<y:
-            list1.pop()
-            list2.pop()
-            break
-        else:
+        if x>y:
+            
             list2.pop()
             
-print(sum(list1))
+            break
+            
+        else:
+            
+            sum=sum+y
+            list2.pop()
+    
+        
+print(sum+list1.pop())
