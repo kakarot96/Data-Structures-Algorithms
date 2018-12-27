a9842657eb71dce34abcd06cf44ffea30423ebae
Kunal Aggarwal <kunalaggarwal58@gmail.com>
2018-12-27 11:26:11 +0000

    A Game of Numbers updated

--- AGameOfNumbers.cpp
+++ AGameOfNumbers.cpp
@@ -1,5 +1,12 @@
 /* 
         Problem Statement:
+        
+        You are given an array A of N integers. Now, two functions F(X) and G(X) are defined:
+        F(X) : This is the smallest number Z such that X < Z <= N and A[X] < A[Z]
+        G(X) : This is the smallest number Z such that X < Z <= N and A[X] > A[Z]
+        Now, you need to find for each index i of this array G(F(i)) , where 1 <= i <= N . If such a number does not exist,
+        for a particular index i, output 1 as its answer. If such a number does exist, output A[G(F(i))]
+
         https://www.hackerearth.com/practice/data-structures/stacks/basics-of-stacks/practice-problems/algorithm/a-game-of-numbers-1-5d3a8cb3
         
 */
