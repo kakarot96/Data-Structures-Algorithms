4391101d289d7fb002fb0473fdf669f8f6109bcf
Kunal Aggarwal <kunalaggarwal58@gmail.com>
2018-12-28 17:25:45 +0000

    Monk and the Order of the Phoenix: Solved

--- Monk&OrderOfThePhoenix.cpp
+++ Monk&OrderOfThePhoenix.cpp
@@ -1,16 +1,8 @@
-/* 
-        Problem Statement:
-        
-        Given an array of positive and negative integers, denoting different types of parentheses. The positive numbers x(i) denotes
-        opening parentheses of type x(i) and negative number -x(i) denotes closing parentheses of type .
-        Open parentheses must be closed by the same type of parentheses. Open parentheses must be closed in the correct order, i.e., 
-        never close an open pair before its inner pair is closed (if it has an inner pair). Thus, [1,2,-2,-1] is balanced, while [1,2,-1,-2] is not balanced.
-        You have to find out the length of the longest subarray that is balanced.
-        
-        https://www.hackerearth.com/practice/data-structures/stacks/basics-of-stacks/practice-problems/algorithm/little-monk-and-balanced-parentheses/
-        
+/*
+    PROBLEM STATEMENT:
+    
+    www.hackerearth.com/practice/data-structures/stacks/basics-of-stacks/practice-problems/algorithm/monk-and-order-of-phoenix/
 */
-
 
 #include<bits/stdc++.h>
 #define ll long long int
@@ -18,14 +10,11 @@ using namespace std;
 multiset<ll, greater <ll> > small;
 
 
-bool specialWand(vector<ll> first, multiset<ll>  stacks[],int n){
-    vector<ll>    :: iterator vitr;
+bool specialWand(vector<ll> &first, multiset<ll> stacks[],int n){
     multiset <ll> :: iterator upper;
     ll smallest = *(--small.end());
     for(int i=1;i<n;i++){
-        upper = upper_bound(stacks[i].begin(), stacks[i].end(), smallest);
-        // cout<<"Smallest is "<<smallest<<endl;
-        //     cout<<"Upper is "<<*upper<<endl;
+         upper = stacks[i].upper_bound(smallest);
         if(*upper <= smallest)
         return false;
         else{
@@ -35,7 +24,8 @@ bool specialWand(vector<ll> first, multiset<ll>  stacks[],int n){
     return true;
 }
 int main(){
-int n;cin>>n;
+    ios_base::sync_with_stdio(false);
+int n;scanf("%d",&n);
 vector<ll> first;
 multiset<ll>  stacks[n+1];  
 multiset <ll> :: iterator sitr; 
@@ -45,29 +35,29 @@ vector<ll>    :: iterator vitr;
 // TAKING INPUT
 for(int i=0;i<n;i++){
     if(i==0){
-        ll num;cin>>num;
+        ll num;scanf("%lld",&num);//cin>>num;
         for(int j=0;j<num;j++){
-            ll temp;cin>>temp;
+            ll temp;scanf("%lld",&temp);//cin>>temp;
             first.push_back(temp);
             small.insert(temp);
         }
     }
     else{
-        int num;cin>>num;
+        int num;scanf("%lld",&num);
         for(int j=0;j<num;j++){
-            ll temp;cin>>temp;
+            ll temp;scanf("%lld",&temp);
             stacks[i].insert(temp);
         }
     }
 }
 
     
-int q,flag=0;cin>>q;
+int q,flag=0;scanf("%d",&q);
 while(q--){
-    cin>>flag;
+    scanf("%d",&flag);
     if(flag==1){
        // cout<<"Insert"<<endl;
-        ll k,h;cin>>k>>h;
+        ll k,h;scanf("%lld%lld",&k,&h);
         if(k==1){
             
             first.push_back(h);
@@ -78,6 +68,7 @@ while(q--){
         }
     }
     else if(flag==2){
+        //specialWand(first,stacks,n);
         if(specialWand(first,stacks,n)){
             cout<<"YES"<<endl;
             
@@ -87,20 +78,18 @@ while(q--){
         
     }
     else if(flag == 0){
-        ll k;cin>>k;
+        ll k;scanf("%lld",&k);
         if(k==1){
             if(!first.empty()){
-                // cout<<"End element is "<<*(--first.end())<<endl;
-                // cout<<"found num is "<<*small.find(*(--first.end()));
-                small.erase(small.find(*(--first.end())));
                 
-                 first.erase (--first.end()); 
+                small.erase(small.find((first[first.size()-1])));
+                first.pop_back(); 
             }
-          //cout<<"erase"<<endl;
+          
         }
         else{
            if(!stacks[k-1].empty()){
-            //cout<<"erase"<<endl;
+            
             stacks[k-1].erase(--stacks[k-1].end());
         } 
         }
@@ -108,33 +97,4 @@ while(q--){
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-// FOR PRINTING ALL ROWS
-    // for(vitr = first.begin(); vitr != first.end() ; ++vitr){
-    //     cout<<'\t'<<*vitr;
-    // }
-    // cout<<endl;
-    // for(int i=1;i<n;i++){
-    //     for ( sitr = stacks[i].begin(); sitr != stacks[i].end(); ++sitr) 
-    // { 
-    //     cout << '\t' << *sitr; 
-    // } 
-    // cout<<endl;
-    // }
-    
-    
-    
-    
-    
 }
