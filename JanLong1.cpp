920bd54c197788f12fbc0091721cb1c816298fb9
Kunal Aggarwal <kunalaggarwal58@gmail.com>
2019-01-07 06:13:17 +0000

    January Long 1

--- JanLong1.cpp
+++ JanLong1.cpp
@@ -1,3 +1,11 @@
+/*
+    Problem Statement:
+    
+    Chef was reading some quotes by great people. Now, he is interested in classifying all the fancy quotes he knows. He thinks that all
+    fancy quotes which contain the word "not" are Real Fancy; quotes that do not contain it are regularly fancy.
+    You are given some quotes. For each quote, you need to tell Chef if it is Real Fancy or just regularly fancy.
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 bool checkNot(string str) 
@@ -6,15 +14,26 @@ bool checkNot(string str)
    { 
        if (x == ' ') 
        { 
-           if(word=="not")
-           word="";
-           return true; 
+           transform(word.begin(), word.end(), word.begin(), ::tolower);
+            //cout<<"word is "<<word<<endl;
+           if(word.compare("not")==0 ){
+             
+            word="";
+           return true;  
+           }
+           else word="";
+
        } 
        else
        { 
-           word = word + x; 
+           word = word + x;
        } 
-   }  
+   }
+              transform(word.begin(), word.end(), word.begin(), ::tolower);
+    if(word.compare("not")==0 ){
+           return true;  
+           }
+
    return false; 
 } 
 int main() {
@@ -26,8 +45,6 @@ int t;cin>>t;
 while(t--){
     
      getline(cin, str); int flag=0;
-   // cout<<str<<endl;
-    
     if(checkNot(str))cout<<"Real Fancy"<<endl;
     else cout<<"regularly fancy"<<endl;
 }
