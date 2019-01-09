8995a056f38ec55f584201437eb2a2d127934996
Kunal Aggarwal <kunalaggarwal58@gmail.com>
2019-01-09 04:39:15 +0000

    January Long 4 : Solved

--- JanLong4.cpp
+++ JanLong4.cpp
@@ -1,20 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int t;cin>>t;
+    int t;scanf("%d",&t);
     while(t--){
-        int n,p,ans=0,idx=0;cin>>n>>p;
+        long long int n,p,ans=0,idx=0;scanf("%lld%lld",&n,&p);
         ans = (n-1)/2;
         if(n%2==0)idx=(n/2)+1;
         else idx=(n+1)/2;
         
-        long int g1=p-n;
-        long int g2=p-ans;
+        long long int g1=p-n;
+        long long int g2=p-ans;
         if(ans==0){
-            cout<<pow(p,3)<<endl;
+            cout<<p*p*p<<endl;
             continue;
         }
-        int count = g1*g1 + g1*g2 + g2*g2;
+        long long int count = g1*g1 + g1*g2 + g2*g2;
          cout<<count<<endl;
         
     }
