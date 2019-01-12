4eb885b742f22893d5218fb1f189e860a2b011a3
saiiiyan <kunalaggarwal58@gmail.com>
2019-01-12 16:39:40 +0530

    LongestCommonSubsequence: Memoization

--- LongestCommonSubsequence.cpp
+++ LongestCommonSubsequence.cpp
@@ -3,21 +3,21 @@ using namespace std;
 string visited[1000][1000];
 string lcs(string s1,string s2){
     
-    string ans="";
+    if(visited[s1.length()][s2.length()]==""){
         
-    if(s1.length()==0 || s2.length()==0)return "";
+    if(s1.length()==0 || s2.length()==0)return visited[s1.length()][s2.length()]="";
     else if(s1[0]==s2[0]){
-        ans+=s1[0]+ lcs(s1.substr(1,s1.length()-1) , s2.substr(1,s2.length()-1));
+        visited[s1.length()][s2.length()]=s1[0]+ lcs(s1.substr(1,s1.length()-1) , s2.substr(1,s2.length()-1));
     }
     else{
         string s3= lcs(s1.substr(1,s1.length()-1) ,s2);
         string s4= lcs(s1 ,s2.substr(1,s2.length()-1));
-        if(s3.length()>s4.length()) ans=s3;
-        else ans=s4;
+        if(s3.length()>s4.length()) visited[s1.length()][s2.length()]=s3;
+        else visited[s1.length()][s2.length()]=s4;
+    }
     }
     
-    
-    return ans;
+    return visited[s1.length()][s2.length()];
 }
 int main(){
 clock_t begin, end;
