0cc51d7dc865800880ff81c1fe83c207e69530ca
Kunal Aggarwal <kunalaggarwal58@gmail.com>
2019-01-17 08:37:01 +0000

    Eerie Planet: Solved

--- EeriePlanet.cpp
+++ EeriePlanet.cpp
@@ -1,3 +1,11 @@
+/*
+    PROBLEM STATEMENT:
+    You own a club on eerie planet. The day on this planet comprises of H hours. You appointed C crew members to handle the huge crowd that you get, being the best club on the planet. Each member of 
+    the crew has fixed number of duty hours to work. There can be multiple or no crew members at work at any given hour of the day.
+    Being on weird planet, the rules of this club cannot be normal. Each member of the crew only allows people who are taller than him to enter the club when he is at work.
+    Given the schedule of work and heights of the crew members, you have to answer Q queries. Each query specifies the time of entry and height of a person who is visiting the club. You have to answer
+    if the person will be allowed to enter the club or not.
+*/
 #include "bits/stdc++.h"
 #define ll long long int;
 using namespace std;
