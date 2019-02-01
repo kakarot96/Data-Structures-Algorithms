56756f243ae50f6ed3f2978bf4b2a143e72a56eb
Kunal Aggarwal <kunalaggarwal58@gmail.com>
2019-02-01 18:51:18 +0000

    February Long 1

--- FebLong1.cpp
+++ FebLong1.cpp
@@ -1,13 +1,18 @@
 #include<bits/stdc++.h>
+#define ll long long int
 using namespace std;
+ll hcf(ll a, ll b){
+    if(b==0)return a;
+    else 
+    return hcf(b,a%b);
+}
 int main(){
     int t;cin>>t;
     while(t--){
-        int n,a,b,k;cin>>n>>a>>b>>k; int count=0;
-        for(int i=1;i<=n;i++){
-             if(i%a==0 and i%b==0)count++;
-        }
-       if(n-count>=k)cout<<"Win"<<endl;
+        ll n,a,b,k;cin>>n>>a>>b>>k; ll count=0,hcfAB;
+        hcfAB =hcf(a,b) ;
+       ll lcm = (a*b)/hcfAB;
+       if((n/a) + (n/b) - (2*(n/lcm))-k>=0)cout<<"Win"<<endl;
        else cout<<"Lose"<<endl;
     }
 }
